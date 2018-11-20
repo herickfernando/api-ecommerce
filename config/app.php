3437ca4d2aab1e417d679f8ab996b4d33742aa53
Herick Fernando Borgo <hericobrogo1@gmail.com>
2018-11-20 19:38:15 -0300

    Added package JWTAuth to authentication.

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
